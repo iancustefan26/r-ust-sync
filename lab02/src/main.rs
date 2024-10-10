2fd7e95d620e82e8b783ee97b159c76cd0289c6e
iancustefan26 <stefaniancu46@gmail.com>
2024-10-10 19:25:24 +0300

    Lab3 problem 1, 2 and 3 (not finished yet)

--- lab02/src/main.rs
+++ lab02/src/main.rs
@@ -1,13 +1,6 @@
 
-fn add_chars_n_ref(s: &mut String, c: char, n: i32) -> (){
-    let mut i = 0 as i32;
-    while i < n{
-        s.push(c);
-        i += 1;
-    }
-}
 
-fn add_chars_n(mut s: String, c: char, n: i32) -> String {
+fn add_chars_n(mut s: String, c: char, n: i32) -> String {  //first problem
     let mut i = 0 as i32;
     while i < n{
         s.push(c);
@@ -17,6 +10,33 @@ fn add_chars_n(mut s: String, c: char, n: i32) -> String {
     s
 }
 
+fn add_chars_n_ref(s: &mut String, c: char, n: i32) -> (){  //second problem
+    let mut i = 0 as i32;
+    while i < n{
+        s.push(c);
+        i += 1;
+    }
+}
+
+fn add_space(s: &mut String) -> (){     //third problem
+    s.push(' ');
+}
+
+fn add_str(s: &mut String, to_add_s: &str) -> (){
+    s.push_str(to_add_s);
+}
+
+fn add_integer(s: &mut String, to_add_integer: i32) -> (){
+    let new_integer_s = to_add_integer.to_string() as String;
+    s.push_str(&new_integer_s);
+}
+
+
+fn add_float(s: &mut String, to_add_float: f64) -> (){
+    let float_as_s: String = to_add_float.to_string();
+    s.push_str(&float_as_s);
+}
+
 fn main() {
     let mut s = String::from("");
     let mut another_s: String = String::from("");
@@ -24,12 +44,20 @@ fn main() {
     let alpha_count: i32 = 26;
     while i < alpha_count {
         let c = (i as u8 + 'a' as u8) as char;
-        s = add_chars_n(s, c, alpha_count - i);
-        add_chars_n_ref(&mut another_s, c, alpha_count - i);
+        s = add_chars_n(s, c, alpha_count - i);  //first problem
+        add_chars_n_ref(&mut another_s, c, alpha_count - i); //second problem
 
         i += 1;
     }
+    add_space(&mut s);
+    print!("{}\n\n{}", s, another_s);   //third problem
+    let example: String = String::from("                                        I 💚
+                                        RUST.
 
-    print!("{}\n\n{}", s, another_s);
+    Most            crate      306_437_968           and     lastest         is
+         downloaded        has             downloads     the         version    2.038.
+                    
+");
+    
 }
 
