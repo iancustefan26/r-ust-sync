38ca399acbd6b48cd22df4576401266e8d985084
iancustefan26 <stefaniancu46@gmail.com>
2024-10-26 23:04:52 +0300

    Problema 2 lab04

--- lab04/src/main.rs
+++ lab04/src/main.rs
@@ -1,5 +1,6 @@
 use std::{fs, io};
 const DUMMY_FILE: &str = "assets/dummy.txt";
+const ERROR_FILE: &str = "assets/fake.txt";
 
 // Problema 1
 fn get_biggest_lines(text: &String) -> Result<(&str, &str), io::Error> {
@@ -23,13 +24,77 @@ fn get_biggest_lines(text: &String) -> Result<(&str, &str), io::Error> {
     Ok((longest_chars, longest_bytes))
 }
 
+// Problema 2
+fn rot13_cipher(input_text: &str) -> Option<String> {
+    for byte in input_text.as_bytes() {
+        if byte.is_ascii() == false {
+            return None;
+        } // Am intampinat un caracter care nu este ascii
+    }
+    let mut cipher: String = String::new();
+    for byte in input_text.as_bytes() {
+        let ch: char;
+        if byte.is_ascii_lowercase() == true {
+            // litera mica
+            ch = (((byte - 97 + 13) % 26) + 97) as char;
+            cipher.push(ch);
+        } else if byte.is_ascii_uppercase() == true {
+            // litera mare
+            ch = (((byte - 65 + 13) % 26) + 65) as char;
+            cipher.push(ch);
+        } else {
+            // orice alt caracter
+            ch = *byte as char;
+            cipher.push(ch);
+        }
+    }
+    Some(cipher)
+}
+
 fn main() {
     // Problema 1
+    // Succes
     let dummy_text: String =
         fs::read_to_string(DUMMY_FILE).expect("Text file path should be valid!");
     let biggest_lines = get_biggest_lines(&dummy_text).expect("io Error");
     println!(
-        "File : {}\nBiggest chars line: {}\nBiggest bytes line: {}",
+        "File : {}\nBiggest chars line: {}\nBiggest bytes line: {}\n",
         DUMMY_FILE, biggest_lines.0, biggest_lines.1
     );
+    // Eroare
+    let error_text = fs::read_to_string(&ERROR_FILE);
+    match error_text {
+        Ok(text) => {
+            let error_case_lines =
+                get_biggest_lines(&text).expect("Error calculating lines length");
+            println!(
+                "File : {}\nBiggest chars line: {}\nBiggest bytes line: {}\n",
+                ERROR_FILE, error_case_lines.0, error_case_lines.1
+            );
+        }
+        Err(e) => println!("File : {ERROR_FILE}\nError : {e}\n"),
+    }
+    // Problema 2
+    // Succes
+    let text = "strings are FUN ?";
+    let cipher;
+    if let Some(cipher_option) = rot13_cipher(&text) {
+        cipher = cipher_option;
+        println!("Text: {}\nROT13 cipher: {}\n", text, cipher);
+    } else {
+        println!("Cannot encode ROT13, the string contains non-ASCII characters.");
+    }
+
+    // Eroare
+    let text = "dsadas 🎁🎶🎉👀🎈🎃🍕☕🍉";
+    let cipher;
+    if let Some(cipher_option) = rot13_cipher(&text) {
+        cipher = cipher_option;
+        println!("Text: {}\nROT13 cipher: {}\n", text, cipher);
+    } else {
+        println!(
+            "Text : {}\nCannot encode ROT13, the string contains non-ASCII characters.\n",
+            text
+        );
+    }
 }
