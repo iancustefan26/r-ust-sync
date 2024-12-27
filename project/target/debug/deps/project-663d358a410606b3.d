3b82a9ac7936564a2ff61377ebb8960079ac4f89
iancustefan26 <stefaniancu46@gmail.com>
2024-12-27 18:17:39 +0200

    Initialized project advanced_rsync

---
+++ project/target/debug/deps/project-663d358a410606b3.d
@@ -0,0 +1,5 @@
+/Users/stefaniancu/Documents/VS Code/Rust/project/target/debug/deps/project-663d358a410606b3: src/main.rs
+
+/Users/stefaniancu/Documents/VS Code/Rust/project/target/debug/deps/project-663d358a410606b3.d: src/main.rs
+
+src/main.rs:
